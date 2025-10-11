38c3f1a428af445596b6e47c8dbef0ccc271fa36
Anay Saha <anaysahapaban@gmail.com>
2025-10-11 12:58:33 +0600

    authcontex

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,9 +1,19 @@
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
 const Register = () => {
   const { register, handleSubmit, formState: {errors} } = useForm();
   
+    const { createUser } = useAuth();
+
           const onSubmit = data =>{
               console.log(data);
+              createUser(data.email, data.password, data.name)
+              .then( result =>{
+                console.log(result.user)
+              })
+              .catch(error=>{
+                console.error(error);
+              })
           }
   
           return (
