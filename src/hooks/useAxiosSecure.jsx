100c9d5545c0535983864d0d29aee8ff85aa37c6
Anay Saha <anaysahapaban@gmail.com>
2025-12-05 03:21:42 +0600

    seq

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,13 @@
 
 import axios from 'axios';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 const axiosSecure = axios.create({
   baseURL: `http://localhost:5000`
 });
 const useAxiosSecure = () => {
-  const {user} = useAuth();
+  const {user, logOut} = useAuth();
+  const navigate = useNavigate();
 
   axiosSecure.interceptors.request.use(config =>{
     config.headers.Authorization =`Bearer ${user.accessToken}`
@@ -13,6 +15,27 @@ const useAxiosSecure = () => {
   }, error =>{
     return Promise.reject(error);
   } )
+
+  axiosSecure.interceptors.response.use(res => {
+    return res;
+  }, error => {
+   
+    const status = error.status;
+    if(status === 403){
+      navigate('/forbidden');
+    }
+    else if (status === 401){
+      logOut()
+      .then(() =>{
+        navigate('/login')
+      })
+
+      .catch(() =>{ })
+      
+    }
+    return Promise.reject(error);
+  } )
+
   return axiosSecure;
 };
 
