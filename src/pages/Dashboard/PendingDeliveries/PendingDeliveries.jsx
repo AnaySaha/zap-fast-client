5656327f85279e7b4df1f9847dee1230af713c30
Anay Saha <anaysahapaban@gmail.com>
2025-12-19 23:28:58 +0600

    add

--- src/pages/Dashboard/PendingDeliveries/PendingDeliveries.jsx
+++ src/pages/Dashboard/PendingDeliveries/PendingDeliveries.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PendingDeliveries = () => {
     return (
         <div>
-            <h1> List of Pending Riders</h1>
+            <h1> Pending Riders</h1>
         </div>
     );
 };
